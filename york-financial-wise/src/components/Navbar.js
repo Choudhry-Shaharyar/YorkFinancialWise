465b4be0c095355a6c7880eb02d1bd4c882ef392
Choudhry-Shaharyar <sheary009@hotmail.com>
2023-12-26 20:57:43 -0500

    added navbar

--- york-financial-wise/src/components/Navbar.js
+++ york-financial-wise/src/components/Navbar.js
@@ -1,7 +1,5 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import {Navbar, Container, Nav, NavDropDown} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 function Navigation() {
   return (
     <>
